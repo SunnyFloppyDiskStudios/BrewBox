dcf2f7cf514027d2a3a77088d9a9de0ecaddb80a
SunnyFlops <arav.prasad@outlook.com>
2026-06-19 10:26:25 +1200

    Initial Commit

---
+++ brewbox/brewboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  brewboxApp.swift
+//  brewbox
+//
+//  Created by SunnyFlops on 19/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct brewboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
